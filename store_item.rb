a3640e4172671ca3115d14f08eb783ca615b0c2b
Sam Tanner <Samuel.m.s.tanner@gmail.com>
2021-02-17 10:25:29 -0800

    creates file and adds 3 items

---
+++ store_item.rb
@@ -0,0 +1,3 @@
+item1 = { name: "candy bar", price: 1, main_ingredient: "chocolate" }
+item2 = { name: "gum", price: 0.5, main_ingredient: "sugar" }
+item3 = { name: "soda", price: 1.3, main_ingredient: "corn syrup" }
